4ca6996a580b61dbd970aaea9a8dff92a32655f2
Alejandro <alejandromartindomingo06@gmail.com>
2025-03-18 12:21:31 +0100

    commit inicial

---
+++ Desktop/Cosas FP/Programacion/cosas eclipse/ejercicioexceptiones/src/ejercicioexceptiones/BurradasNOException.java
@@ -0,0 +1,13 @@
+package ejercicioexceptiones;
+
+/**
+ * Creamos la exception personalizada 
+ * @param la exception y su mensaje
+ */ 
+class BurradasNOException extends Exception {
+    public BurradasNOException(String tpalabros) {
+        super("Error no se permite lenguaje obsceno = "+  tpalabros);
+    }
+}
+	
+
